0b506694d049468fe07b55b6e5a5655d169928f5
hollaze <deperiers.a@gmail.com>
2021-10-13 22:38:38 +0200

    basic window with rendering

--- src/main.c
+++ src/main.c
@@ -2,9 +2,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void exit_with_error(const char *message)
+{
+	SDL_Log("Error: %s > %s", message, SDL_GetError());
+	SDL_Quit();
+	exit(EXIT_FAILURE);
+}
+
 int main(void)
 {
 	SDL_Window *window = NULL;
+	SDL_Renderer *renderer = NULL;
 
 	/* Cannot Initialize SDL video */
 	if (SDL_Init(SDL_INIT_VIDEO) != 0)
@@ -13,19 +21,18 @@ int main(void)
 		exit(EXIT_FAILURE);
 	}
 
-	/* Title, x, y, width, height, flags -> window type */
-	window = SDL_CreateWindow("Première fenêtre SDL 2",
-				  SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-				  800, 600, SDL_WINDOW_MINIMIZED);
+	/* width, height, window flag, window, renderer */
+	if (SDL_CreateWindowAndRenderer(800, 600, SDL_WINDOW_MINIMIZED, &window, &renderer) != 0)
+		exit_with_error("Cannot create window and renderer");
+	SDL_SetWindowTitle(window, "The Maze");
 
-	if (window == NULL)
-	{
-		SDL_Log("ERREUR : Creation fenêtre échouée > %s\n", SDL_GetError());
-		exit(EXIT_FAILURE);
-	}
 
 	/* Window delay */
 	SDL_Delay(5000);
+	/* Free window */
+	SDL_DestroyWindow(window);
+	/* Free renderer */
+	SDL_DestroyRenderer(renderer);
 
 	SDL_Quit();
 
