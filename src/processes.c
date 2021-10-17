5e487810c31814708f30e50740c58e3940fc0eb1
hollaze <deperiers.a@gmail.com>
2021-10-17 19:00:24 +0200

    Fixed without using extern int variable

--- src/processes.c
+++ src/processes.c
@@ -2,20 +2,23 @@
 #include <stdlib.h>
 #include "maze.h"
 
-void processQuitAndEscape()
+int processQuitAndEscape(is_game_running game_running, int running)
 {
 	SDL_Event event;
+	running = game_running.running;
+
 	SDL_PollEvent(&event);
 
 	switch (event.type)
 	{
 	case SDL_QUIT:
-		isGameRunning = FALSE;
+		running = FALSE;
 		break;
 
 	case SDL_KEYDOWN:
 		if (event.key.keysym.sym == SDLK_ESCAPE)
-			isGameRunning = FALSE;
+			running = FALSE;
 		break;
 	}
+	return (running);
 }
