2ca2d30e6afd7639378c9a26983ea0f3b472e4d1
hollaze <deperiers.a@gmail.com>
2021-11-02 09:39:30 +0100

    colors for under the PNG image, the ceiling and the floor

--- src/color_buffer.c
+++ src/color_buffer.c
@@ -1,28 +1,34 @@
 #include "../headers/maze.h"
 #include <stdlib.h>
 
-void setupColorBuffer(void)
+void allocateColorBuffer(void)
 {
-        color_buffer = (Uint32 *)malloc(sizeof(Uint32) *
-                                        (Uint32)WINDOW_WIDTH *
-                                        (Uint32)WINDOW_HEIGHT);
-
-        color_buffer_texture = SDL_CreateTexture(renderer,
-                                                 SDL_PIXELFORMAT_ARGB8888,
-                                                 SDL_TEXTUREACCESS_STREAMING,
-                                                 WINDOW_WIDTH,
-                                                 WINDOW_HEIGHT);
+	/* allocate enough memory to hole the entire screen inside the buffer */
+	r.color_buffer = (uint32_t *)malloc(sizeof(uint32_t) *
+					    WINDOW_WIDTH *
+					    WINDOW_HEIGHT);
+	if (r.color_buffer == NULL)
+		exitWithError("allocateColorBuffer, color_buffer is NULL");
 }
 
-void clearColorBuffer(Uint32 color)
-{
-        int x, y;
+/**
+ * clearColorBuffer - store the color of the walls in the buffer
+ *
+ * @color: the color of the walls
+ * @color_buffer: the buffer of colors
+ *
+ * Return: color_buffer
+ */
 
-        for (x = 0; x < WINDOW_WIDTH; x++)
-        {
-                for (y = 0; y < WINDOW_HEIGHT; y++)
-                {
-                        color_buffer[(WINDOW_WIDTH * y) + x] = color;
-                }
-        }
+void clearColorBuffer(uint32_t color)
+{
+	int x, y;
+
+	for (x = 0; x < WINDOW_WIDTH; x++)
+	{
+		for (y = 0; y < WINDOW_HEIGHT; y++)
+		{
+			r.color_buffer[(WINDOW_WIDTH * y) + x] = color;
+		}
+	}
 }
