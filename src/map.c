637a5ce6b61bbe643165835644ffb93854f8dc6c
hollaze <deperiers.a@gmail.com>
2021-11-03 18:38:44 +0100

    map of the game

--- src/map.c
+++ src/map.c
@@ -2,7 +2,7 @@
 
 /* map: extern variable, map of the game */
 /* 0 for void */
-/* >0 for walls */
+/* != 0 for walls */
 
 const int map[MAP_NUM_ROWS][MAP_NUM_COLS] = {
 {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
